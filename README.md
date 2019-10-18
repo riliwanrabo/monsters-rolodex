ea94bccd167dfe9636cf0556ef7a2f8c6ea1c50c
riliwan balogun <riliwan.rabo@gmail.com>
2019-10-18 16:39:09 +0100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## Welcome to my monsters rolodex
-
-Kindly sit back and relax to find your favorite monters
---
+++ precache-manifest.7869b661499fe244df7bc833fe3489d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc2f2150c0cf20273db1ad2c9d77feb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fec605e1cdf49fa46c39",
+    "url": "/monsters-rolodex/static/css/main.0e7cd3fe.chunk.css"
+  },
+  {
+    "revision": "a7ffd671cb21d8ff7fb4",
+    "url": "/monsters-rolodex/static/js/2.fe5198a4.chunk.js"
+  },
+  {
+    "revision": "fec605e1cdf49fa46c39",
+    "url": "/monsters-rolodex/static/js/main.84dba12a.chunk.js"
+  },
+  {
+    "revision": "d78c7e54dca26e3c25e2",
+    "url": "/monsters-rolodex/static/js/runtime-main.aaf9d0bd.js"
+  }
+]);
